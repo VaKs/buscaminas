bb6bae55b1b0e3e374560c6ea50f8d4400fb151b
VaKs <VaKs@10.236.58.38>
2017-06-11 16:23:27 +0200

    TERMINADO

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,9 @@
 
 import Presentacion.FrameBuscaminas;
 
-
-
-
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-        
         FrameBuscaminas fb= new FrameBuscaminas();
     }
 
