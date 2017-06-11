93583c2f1a30a8a03a105666541364b0818b64d7
VaKs <VaKs@10.236.58.38>
2017-06-11 16:25:02 +0200

    TERMINADO

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import Presentacion.FrameBuscaminas;
 public class Main {
 
     public static void main(String[] args) {
-        FrameBuscaminas fb= new FrameBuscaminas();
+        FrameBuscaminas juegoBuscaminas= new FrameBuscaminas();
     }
 
 }
